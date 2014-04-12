3615043bcf94f50755ab95587a9fa199cb5b4eeb
Will Long <will.long043@gmail.com>
2014-04-12 13:46:27 -0400

    Moved process attaching to TaskProbe.

--- dbg.cpp
+++ dbg.cpp
@@ -3,10 +3,28 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+class TaskProbe {
+	public:
+		TaskProbe();
+		int attach_to_process(int);
+	private:
+		thread_act_port_array_t thread_list;
+		mach_msg_type_number_t threadCount;
+		mach_port_t task;
+};
+
+TaskProbe::TaskProbe() {
+	return;
+}
+
+int TaskProbe::attach_to_process(int pid) {
+	int machout = task_for_pid(mach_task_self(), pid, &task);
+	printf("taskforpid %s\n", mach_error_string(machout));
+	return 0;
+}
+
+
 int main(int argc, char * argv[]) {
-	thread_act_port_array_t thread_list;
-	mach_msg_type_number_t threadCount;
-	mach_port_t task;
 	int pid = fork();
 	if (pid == 0)
 	{
@@ -16,8 +34,8 @@ int main(int argc, char * argv[]) {
 	else
 	{
 		printf("I'm the parent, my child is %i\n", pid);
-		int machout = task_for_pid(mach_task_self(), pid, &task);
-		printf("taskforpid %s\n", mach_error_string(machout));
+		TaskProbe t = TaskProbe();
+		t.attach_to_process(pid);
 	}
 	return 0;
 }
