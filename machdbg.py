b75704378a413f987299835cfeff4e99e6f77c80
Will Long <will.long043@gmail.com>
2014-04-13 16:51:22 -0400

    added command line stuff to machdbg.py

--- machdbg.py
+++ machdbg.py
@@ -1,10 +1,36 @@
-import os, sys
+import os, sys, argparse
 from subprocess import *
 import time
 from ctypes import *
 
-if __name__ == '__main__':
+libc = None
+
+#commands = ["run", "quit"]
+
+def parse_command(tokens):
+	if(tokens[0] == "quit"):
+		sys.exit(0)
+
+def main():
 	libc = CDLL('libc.dylib')
 	if not libc:
-		print "Not working"
-		raise Exception("Not working")
+		print "Failed to load C Library"
+		raise Exception("Failed to load C Library")
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('executable', nargs='?', help='executable help')
+	args = parser.parse_args()
+	executable = args.executable
+	if executable == None:
+		print "No executable"
+	else:
+		print "Gave executable " + executable
+
+	while True:
+		command = raw_input("(mdbg) ")
+		tokens = command.split(' ')
+		print tokens[0]
+		if tokens[0] in ["run", "quit"]:
+			parse_command(tokens)
+
+if __name__ == '__main__': main()
